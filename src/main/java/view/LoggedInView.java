20821574ad3bcc55dc725dafd5db4c618d5236a1
Arsen <arsenii.krokhin@mail.utoronto.ca>
2025-10-20 09:56:21 -0400

    saved logout controller in instance variable and executed logout use case through the controller

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -108,7 +108,9 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
      * @param evt the ActionEvent to react to
      */
     public void actionPerformed(ActionEvent evt) {
-        System.out.println("Click " + evt.getActionCommand());
+        if (evt.getSource() == logOut) {
+            logoutController.execute();
+        }
     }
 
     @Override
@@ -139,6 +141,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     }
 
     public void setLogoutController(LogoutController logoutController) {
-        // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
